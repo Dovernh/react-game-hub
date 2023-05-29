5513067acc9974f9ad032110ec250bf44096af9c
Paul Harriman <dovernh@gmail.com>
2023-05-29 11:24:04 -0400

    display genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,13 +1,25 @@
+import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
 import { useGenres } from "../hooks/useGenres";
+import { getCroppedImageUrl } from "../services/image-url";
 
 export const GenreList = () => {
   const { data } = useGenres();
 
   return (
-    <ul>
+    <List>
       {data.map((d) => (
-        <li key={d.id}>{d.name}</li>
+        <ListItem key={d.id} paddingY="5px">
+          <HStack>
+            <Image
+              boxSize="32px"
+              borderRadius={8}
+              src={getCroppedImageUrl(d.image_background)}
+            />
+
+            <Text fontSize="large">{d.name}</Text>
+          </HStack>
+        </ListItem>
       ))}
-    </ul>
+    </List>
   );
 };
